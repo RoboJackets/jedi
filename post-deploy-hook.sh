7a8d9e5f089e3fe444f0321447660365905fe041
kyla qi <kqi12345@gmail.com>
2019-03-11 18:29:27 -0400

    add deploy hooks that are maybe broken

---
+++ post-deploy-hook.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd "${0%/*}"
+
+composer install --no-interaction --no-progress --no-suggest
+php artisan migrate --no-interaction
+php artisan config:clear --no-interaction
+php artisan view:clear --no-interaction
+php artisan route:clear --no-interaction
+php artisan vendor:publish --provider="Laravel\Horizon\HorizonServiceProvider" --no-interaction
+php artisan cache:clear --no-interaction
+npm install --no-progress
+npm run production --no-progress
+php artisan up
+php artisan horizon:terminate
---
+++ pre-deploy-hook.sh
@@ -0,0 +1,3 @@
+cd "${0%/*}"
+
+php artisan down --message="An app upgrade is in progress. Please try again in a few minutes." --retry=60
