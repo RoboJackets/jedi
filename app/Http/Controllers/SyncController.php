bf68b0315604117681e922838d98ade905ac8b87
Kristaps Berzinch <kristapsberzinch@gmail.com>
2019-07-02 12:58:13 -0500

    stan

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Jobs\SyncNextcloud;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class SyncController extends Controller
 {
