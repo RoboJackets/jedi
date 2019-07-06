9a0a93e0dbf8720ecb13fb3896e2fb8ed6baca7d
Kristaps Berzinch <kristapsberzinch@gmail.com>
2019-07-06 13:21:34 -0500

    Wrap entire Vault job in a try-catch for session expiration

--- app/Jobs/SyncVault.php
+++ app/Jobs/SyncVault.php
@@ -20,6 +20,15 @@ class SyncVault extends AbstractSyncJob
     public $queue = 'vault';
 
     /**
+     * The number of times the job may be attempted.
+     *
+     * Overridden to 2 here because we want to retry if the issue was with the security header.
+     *
+     * @var int
+     */
+    public $tries = 2;
+
+    /**
      * Execute the job.
      *
      * @return void
@@ -53,117 +62,108 @@ class SyncVault extends AbstractSyncJob
 
         $userId = Cache::get('vault_user_id_' . $this->uid);
 
-        if (null === $userId) {
-            $userId = $vault->getUserIdByUsername($this->uid);
+        try {
+            if (null === $userId) {
+                $userId = $vault->getUserIdByUsername($this->uid);
 
-            if (-1 !== $userId) {
-                Cache::forever('vault_user_id_' . $this->uid, $userId);
-            }
-        } else {
-            $userId = intval($userId);
-        }
-
-        if (-1 === $userId) {
-            Log::info(self::class . ': User ' . $this->uid . ' does not exist in Vault');
-
-            return;
-        }
-
-        $user = $vault->getUserByUserId($userId);
-
-        if ($user->Name !== $this->uid) {
-            throw new Exception('Cached ID has wrong username');
-        }
-
-        if ($this->is_access_active) {
-            Log::info(self::class . ': Enabling user ' . $this->uid);
-            $need_to_save = false;
-
-            if (true !== $user->IsActive) {
-                $user->IsActive = true;
-                $need_to_save = true;
-            }
-
-            if ($user->FirstName !== $this->first_name) {
-                $user->FirstName = $this->first_name;
-                $need_to_save = true;
-            }
-
-            if ($user->LastName !== $this->last_name) {
-                $user->LastName = $this->last_name;
-                $need_to_save = true;
-            }
-
-            if ('' !== $user->Email) {
-                $user->Email = '';
-                $need_to_save = true;
-            }
-
-            if ($need_to_save) {
-                Log::debug(self::class . ': Updating name/email for user ' . $this->uid);
-                $user->save();
+                if (-1 !== $userId) {
+                    Cache::forever('vault_user_id_' . $this->uid, $userId);
+                }
             } else {
-                Log::debug(self::class . ': User ' . $this->uid . ' attributes are up to date');
+                $userId = intval($userId);
             }
 
-            $groups = Cache::get('vault_groups');
+            if (-1 === $userId) {
+                Log::info(self::class . ': User ' . $this->uid . ' does not exist in Vault');
 
-            if (null === $groups) {
-                $groups = $vault->getAllGroups();
-                Cache::forever('vault_groups', $groups);
+                return;
             }
 
-            foreach ($this->teams as $team) {
-                $filteredgroups = array_filter(
-                    $groups,
-                    static function (Group $group) use ($team): bool {
-                        return $group->Name === $team && $group->IsActive;
+            $user = $vault->getUserByUserId($userId);
+
+            if ($user->Name !== $this->uid) {
+                throw new Exception('Cached ID has wrong username');
+            }
+
+            if ($this->is_access_active) {
+                Log::info(self::class . ': Enabling user ' . $this->uid);
+                $need_to_save = false;
+
+                if (true !== $user->IsActive) {
+                    $user->IsActive = true;
+                    $need_to_save = true;
+                }
+
+                if ($user->FirstName !== $this->first_name) {
+                    $user->FirstName = $this->first_name;
+                    $need_to_save = true;
+                }
+
+                if ($user->LastName !== $this->last_name) {
+                    $user->LastName = $this->last_name;
+                    $need_to_save = true;
+                }
+
+                if ('' !== $user->Email) {
+                    $user->Email = '';
+                    $need_to_save = true;
+                }
+
+                if ($need_to_save) {
+                    Log::debug(self::class . ': Updating name/email for user ' . $this->uid);
+                    $user->save();
+                } else {
+                    Log::debug(self::class . ': User ' . $this->uid . ' attributes are up to date');
+                }
+
+                $groups = Cache::get('vault_groups');
+
+                if (null === $groups) {
+                    $groups = $vault->getAllGroups();
+                    Cache::forever('vault_groups', $groups);
+                }
+
+                foreach ($this->teams as $team) {
+                    $filteredgroups = array_filter(
+                        $groups,
+                        static function (Group $group) use ($team): bool {
+                            return $group->Name === $team && $group->IsActive;
+                        }
+                    );
+
+                    if (0 === count($filteredgroups)) {
+                        continue;
                     }
-                );
 
-                if (0 === count($filteredgroups)) {
-                    continue;
+                    if (1 === count($filteredgroups)) {
+                        // checking group membership is ass so we're just going to try to add them
+                        // as far as i can tell this doesn't throw an exception or anything if they're already in the group
+                        $key = array_keys($filteredgroups)[0];
+                        Log::debug(self::class . ': Adding group ' . $filteredgroups[$key]->Name . ' to ' . $this->uid);
+                        $vault->addUserToGroup($userId, $filteredgroups[$key]->Id);
+                    }
+
+                    if (count($filteredgroups) > 1) {
+                        throw new Exception('Two groups with the same name, y u do dis');
+                    }
                 }
-
-                if (1 === count($filteredgroups)) {
-                    // checking group membership is ass so we're just going to try to add them
-                    // as far as i can tell this doesn't throw an exception or anything if they're already in the group
-                    $key = array_keys($filteredgroups)[0];
-                    Log::debug(self::class . ': Adding group ' . $filteredgroups[$key]->Name . ' to ' . $this->uid);
-                    $vault->addUserToGroup($userId, $filteredgroups[$key]->Id);
-                }
-
-                if (count($filteredgroups) > 1) {
-                    throw new Exception('Two groups with the same name, y u do dis');
-                }
-            }
-        } else {
-            if ($user->IsActive) {
-                Log::info(self::class . ': Disabling user ' . $this->uid);
-
-                $user->IsActive = false;
-                $user->save();
-
-                Log::info(self::class . ': Successfully disabled ' . $this->uid);
             } else {
-                Log::info(self::class . ': User ' . $this->uid . ' already disabled, don\'t need to change anything');
+                if ($user->IsActive) {
+                    Log::info(self::class . ': Disabling user ' . $this->uid);
+
+                    $user->IsActive = false;
+                    $user->save();
+
+                    Log::info(self::class . ': Successfully disabled ' . $this->uid);
+                } else {
+                    Log::info(self::class . ': User ' . $this->uid . ' already disabled, don\'t need to change anything');
+                }
             }
-        }
-    }
-
-    // phpcs:disable SlevomatCodingStandard.ControlStructures.EarlyExit.EarlyExitNotUsed
-
-    /**
-     * The job failed to process.
-     *
-     * @param Throwable  $throwable
-     *
-     * @return void
-     */
-    public function failed(Throwable $throwable): void
-    {
-        if ($throwable instanceof SoapFault && 300 === intval($throwable->getMessage())) {
-            Cache::forget('vault_security_header');
+        } catch (SoapFault $e) {
+            if (300 === intval($e->getMessage())) {
+                Cache::forget('vault_security_header');
+            }
+            throw $e;
         }
     }
 }
