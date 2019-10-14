3c1fcb56b30d8282ff600ecf01e6f91b6d2f6e20
Kristaps Berzinch <kristapsberzinch@gmail.com>
2019-10-14 17:06:25 -0400

    Fix default value for SUMS_WHITELISTED_ACCOUNTS

--- config/sums.php
+++ config/sums.php
@@ -3,7 +3,7 @@
 return [
     'enabled' => env('SUMS_ENABLED', false),
     'server' => env('SUMS_SERVER'),
-    'whitelisted_accounts' => explode(',', env('SUMS_WHITELISTED_ACCOUNTS')),
+    'whitelisted_accounts' => explode(',', env('SUMS_WHITELISTED_ACCOUNTS', '')),
     'token' => env('SUMS_TOKEN'),
     'billinggroupid' => env('SUMS_BILLING_GROUP_ID'),
     'attendance_timeout_enabled' => env('SUMS_ATTENDANCE_TIMEOUT_ENABLED', false),
