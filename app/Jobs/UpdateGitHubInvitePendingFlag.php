c919d89780c96851e5f583c862c54b5284dcf6db
Kristaps Berzinch <kristapsberzinch@gmail.com>
2019-10-24 00:15:15 -0400

    cleanup

--- app/Jobs/UpdateGitHubInvitePendingFlag.php
+++ app/Jobs/UpdateGitHubInvitePendingFlag.php
@@ -41,7 +41,7 @@ class UpdateGitHubInvitePendingFlag extends AbstractApiaryJob
             '/api/v1/users/' . $this->uid,
             [
                 'json' => [
-                    'github_invite_pending' => $this->github_invite_pending ? 'true' : 'false',
+                    'github_invite_pending' => $this->github_invite_pending,
                 ],
             ]
         );
