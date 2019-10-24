e8cd4832b4948346e78b6270cb20f7bb3fdedb2c
Kristaps Berzinch <kristapsberzinch@gmail.com>
2019-10-23 23:04:30 -0400

    semicolon

--- app/SignatureValidators/GitHub.php
+++ app/SignatureValidators/GitHub.php
@@ -9,7 +9,7 @@ class GitHub implements SignatureValidator
 {
     public function isValid(Request $request, WebhookConfig $config): bool
     {
-        [$algo, $hash] = explode($request->header('X-Hub-Signature'))
+        [$algo, $hash] = explode($request->header('X-Hub-Signature'));
 
         return $hash === hash_hmac($algo, $request->getContent(), $config->signingSecret);
     }
