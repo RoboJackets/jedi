87720eecacd1100f78ba531745e6f661902ae349
Kristaps Berzinch <kristapsberzinch@gmail.com>
2019-10-23 22:44:09 -0400

    semicolon

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1);
 
-use App\Http\Controllers\SyncController
+use App\Http\Controllers\SyncController;
 
 /*
 |--------------------------------------------------------------------------
