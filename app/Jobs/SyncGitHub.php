b40c264407472e5180c59616931ebdeb40c68912
Kristaps Berzinch <kristapsberzinch@gmail.com>
2019-11-17 16:41:20 -0500

    Detect renamed accounts when adding/removing from organization

--- app/Jobs/SyncGitHub.php
+++ app/Jobs/SyncGitHub.php
@@ -161,6 +161,12 @@ class SyncGitHub extends AbstractSyncJob
                 $response = $client->get('/users/' . $this->github_username);
 
                 if (200 !== $response->getStatusCode()) {
+                    if (404 === $response->getStatusCode()) {
+                        throw new Exception(
+                            'Linked account does not exist; it may have been renamed. Administrator '
+                            . 'investigation required! Response from GitHub: ' . $response->getBody()->getContents()
+                        )
+                    }
                     throw new Exception(
                         'GitHub returned an unexpected HTTP response code ' . $response->getStatusCode()
                             . ', expected 200 - ' . $response->getBody()->getContents()
@@ -255,6 +261,22 @@ class SyncGitHub extends AbstractSyncJob
         }
 
         if (404 === $response->getStatusCode()) {
+            $this->info('GitHub says user is not in the organization, making sure the account exists...')
+            $response = $client->get('/users/' . $this->github_username);
+
+            if (200 !== $response->getStatusCode()) {
+                if (404 === $response->getStatusCode()) {
+                    throw new Exception(
+                        'Linked account does not exist; it may have been renamed. Administrator '
+                        . 'investigation required! Response from GitHub: ' . $response->getBody()->getContents()
+                    )
+                }
+                throw new Exception(
+                    'GitHub returned an unexpected HTTP response code ' . $response->getStatusCode()
+                        . ', expected 200 - ' . $response->getBody()->getContents()
+                );
+            }
+
             $this->info('not a member and shouldn\'t be - nothing to do');
             return;
         }
