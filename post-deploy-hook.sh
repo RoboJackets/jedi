a6cdc40f3f892dba6fab8855222ca0ba15a8c115
Kristaps Berzinch <kristapsberzinch@gmail.com>
2019-11-24 16:03:27 -0500

    Put horizon at default path

--- post-deploy-hook.sh
+++ post-deploy-hook.sh
@@ -9,10 +9,6 @@ php artisan view:cache --no-interaction
 php artisan route:cache --no-interaction
 php artisan vendor:publish --provider="Laravel\Horizon\HorizonServiceProvider" --no-interaction
 php artisan cache:clear --no-interaction
-# Patch Horizon not cooperating with being at /
-sed -i 's#base:"/"+window.Horizon.path+"/"#base:"/"#' public/vendor/horizon/app.js
-sed -i 's#"/"+Horizon.path+##g' public/vendor/horizon/app.js
-sed -i 's#"/"+t.Horizon.path+##g' public/vendor/horizon/app.js
 php artisan up
 php artisan horizon:terminate
 php artisan bugsnag:deploy --repository "https://github.gatech.edu/RoboJackets/jedi" --revision $(git rev-parse HEAD) --provider "github-enterprise" --builder "rj-dc-00"
