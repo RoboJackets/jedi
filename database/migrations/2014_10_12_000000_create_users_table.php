c33212bdf6ca26baabeafb801a9082e8d3eafe61
Kristaps Berzinch <kristapsberzinch@gmail.com>
2020-02-15 15:18:35 -0500

    Suppress Phan warning

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+// @phan-suppress-next-line PhanRedefineClass
 class CreateUsersTable extends Migration
 {
     /**
