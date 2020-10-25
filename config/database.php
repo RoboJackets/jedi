099cb40e9682b022cccbb4b31140ae91ad47d29d
Kristaps Berzinch <kristapsberzinch@gmail.com>
2020-10-25 13:51:41 -0400

    Remove unneeded whitespace

--- config/database.php
+++ config/database.php
@@ -128,9 +128,7 @@ return [
 
         'default' => [
             'scheme' => env('REDIS_SCHEME'),
-
             'path' => env('REDIS_PATH'),
-
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
@@ -140,9 +138,7 @@ return [
 
         'cache' => [
             'scheme' => env('REDIS_SCHEME'),
-
             'path' => env('REDIS_PATH'),
-
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
