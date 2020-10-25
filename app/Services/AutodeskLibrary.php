40f809c5e230f0fcd34187674ec0dd1e9142a5ef
Kristaps Berzinch <kristapsberzinch@gmail.com>
2020-10-25 13:06:09 -0400

    phpstan

--- app/Services/AutodeskLibrary.php
+++ app/Services/AutodeskLibrary.php
@@ -4,6 +4,35 @@ declare(strict_types=1);
 
 namespace App\Services;
 
+use GuzzleHttp\Client;
+
 class AutodeskLibrary extends Service
 {
+    /**
+     * A Guzzle client configured for Autodesk Library
+     *
+     * @var \GuzzleHttp\Client
+     */
+    private static $client;
+
+    public static function client(): Client
+    {
+        if (null !== self::$client) {
+            return self::$client;
+        }
+
+        self::$client = new Client(
+            [
+                'base_uri' => config('apiary.server'),
+                'headers' => [
+                    'User-Agent' => 'JEDI on ' . config('app.url'),
+                    'Authorization' => 'Bearer ' . config('apiary.token'),
+                    'Accept' => 'application/json',
+                ],
+                'allow_redirects' => false,
+            ]
+        );
+
+        return self::$client;
+    }
 }
