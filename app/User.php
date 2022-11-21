81ffe1b31fc45bb9e3c424c5c165df709d50454a
Kristaps Berzinch <kristapsberzinch@gmail.com>
2022-11-21 16:22:46 -0600

    Remove unneeded use

--- app/User.php
+++ app/User.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
