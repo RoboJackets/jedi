dcf2cba989e96b53e621d1ba0ed51afb7587d39e
Kristaps Berzinch <kristapsberzinch@gmail.com>
2022-11-21 16:32:51 -0600

    Bump CAS packages

--- config/cas.php
+++ config/cas.php
@@ -10,6 +10,7 @@ return [
     | Example: 'cas.myuniv.edu'.
     */
     'cas_hostname' => env('CAS_HOSTNAME', 'cas.myuniv.edu'),
+
     /*
     |--------------------------------------------------------------------------
     | CAS Authorized Hosts
@@ -19,12 +20,15 @@ return [
     | balanced hosts, then separate each with a comma.
     */
     'cas_real_hosts' => env('CAS_REAL_HOSTS', 'cas.myuniv.edu'),
+
+
     /*
     |--------------------------------------------------------------------------
     | Customize CAS Session Cookie Name
     |--------------------------------------------------------------------------
     */
     'cas_session_name' => env('CAS_SESSION_NAME', 'CASAuth'),
+
     /*
     |--------------------------------------------------------------------------
     | Laravel has it's own authentication sessions. Unless you want phpCAS
@@ -34,12 +38,14 @@ return [
     |--------------------------------------------------------------------------
     */
     'cas_control_session' => env('CAS_CONTROL_SESSIONS', false),
+
     /*
     |--------------------------------------------------------------------------
     | Enable using this as a cas proxy
     |--------------------------------------------------------------------------
     */
     'cas_proxy' => env('CAS_PROXY', false),
+
     /*
     |--------------------------------------------------------------------------
     | Cas Port
@@ -47,6 +53,7 @@ return [
     | Usually 443
     */
     'cas_port' => env('CAS_PORT', 443),
+
     /*
     |--------------------------------------------------------------------------
     | CAS URI
@@ -54,6 +61,16 @@ return [
     | Sometimes is /cas
     */
     'cas_uri' => env('CAS_URI', '/cas'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Client service: host, port, protocol.
+    |--------------------------------------------------------------------------
+    | Example: 'http://localhost', 'https://example.com:8888'
+    */
+    'cas_client_service' => env('CAS_CLIENT_SERVICE', 'http://localhost'),
+
+
     /*
     |--------------------------------------------------------------------------
     | CAS Validation
@@ -64,6 +81,7 @@ return [
     | VALIDATING THE CAS SERVER IS CRUCIAL TO THE SECURITY OF THE CAS PROTOCOL
     */
     'cas_validation' => env('CAS_VALIDATION', ''),
+
     /*
     |--------------------------------------------------------------------------
     | CA Certificate
@@ -72,6 +90,7 @@ return [
     | the CA certificate that is the issuer of the cert
     */
     'cas_cert' => env('CAS_CERT', ''),
+
     /*
     |--------------------------------------------------------------------------
     | CN Validation (if you are using CA certs)
@@ -81,6 +100,7 @@ return [
     | this set with default (true).
     */
     'cas_validate_cn' => env('CAS_VALIDATE_CN', true),
+
     /*
     |--------------------------------------------------------------------------
     | CAS Login URI
@@ -88,12 +108,14 @@ return [
     | Empty is fine
     */
     'cas_login_url' => env('CAS_LOGIN_URL', ''),
+
     /*
     |--------------------------------------------------------------------------
     | CAS Logout URI
     |--------------------------------------------------------------------------
     */
     'cas_logout_url' => env('CAS_LOGOUT_URL', 'https://cas.myuniv.edu/cas/logout'),
+
     /*
     |--------------------------------------------------------------------------
     | CAS Logout Redirect Services
@@ -102,6 +124,7 @@ return [
     | in this section.  If left blank, it will default to disabled.
     */
     'cas_logout_redirect' => env('CAS_LOGOUT_REDIRECT', ''),
+
     /*
     |--------------------------------------------------------------------------
     | CAS Successful Logon Redirection Url
@@ -111,6 +134,7 @@ return [
     | and force the user to be redirected to a specific URL here.
     */
     'cas_redirect_path' => env('CAS_REDIRECT_PATH', ''),
+
     /*
     |--------------------------------------------------------------------------
     | CAS Supports SAML 1.1, allowing you to retrieve more than just the
@@ -119,6 +143,7 @@ return [
     |--------------------------------------------------------------------------
     */
     'cas_enable_saml' => env('CAS_ENABLE_SAML', true),
+
     /*
     |--------------------------------------------------------------------------
     | CAS will support version 1.0, 2.0, 3.0 of the protocol.  It is recommended
@@ -126,7 +151,7 @@ return [
     | will override this configuration.
     |--------------------------------------------------------------------------
     */
-    'cas_version' => env('CAS_VERSION', '2.0'),
+    'cas_version' => env('CAS_VERSION', "2.0"),
 
     /*
     |--------------------------------------------------------------------------
@@ -138,6 +163,7 @@ return [
     |--------------------------------------------------------------------------
     */
     'cas_debug' => env('CAS_DEBUG', false),
+
     /*
     |--------------------------------------------------------------------------
     | Enable Verbose error messages. Not recommended for production.
@@ -145,6 +171,7 @@ return [
     |--------------------------------------------------------------------------
     */
     'cas_verbose_errors' => env('CAS_VERBOSE_ERRORS', false),
+
     /*
     |--------------------------------------------------------------------------
     | This will cause CAS to skip authentication and assume this user id.
@@ -152,4 +179,16 @@ return [
     | will return null in this condition.
      */
     'cas_masquerade' => env('CAS_MASQUERADE', ''),
+
+    /*
+    |--------------------------------------------------------------------------
+    | The value to set in the Domain field on cookies.
+     */
+    'cas_session_domain' => env('APP_DOMAIN', ''),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Whether to add the Secure flag to cookies.
+     */
+    'cas_session_secure' => env('HTTPS_ONLY_COOKIES', false),
 ];
