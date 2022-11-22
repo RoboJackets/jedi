111c5a4cf09fa49b9cde9a184edd6232ba5c368a
Kristaps Berzinch <kristapsberzinch@gmail.com>
2022-11-22 15:43:37 -0500

    Create github_webook_calls table

---
+++ database/migrations/2022_11_22_204253_create_github_webhook_calls_table.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('github_webhook_calls', static function (Blueprint $table): void {
+            $table->bigIncrements('id');
+
+            $table->string('url');
+            $table->string('name');
+            $table->json('headers')->nullable();
+            $table->json('payload')->nullable();
+            $table->text('exception')->nullable();
+
+            $table->timestamps();
+        });
+    }
+};
