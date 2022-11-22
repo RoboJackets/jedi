71f49ebde9c08f382bbe1474d68d8c39f83627b6
Kristaps Berzinch <kristapsberzinch@gmail.com>
2022-11-22 11:26:38 -0500

    Default GOOGLE_CREDENTIALS to empty map for build

--- config/google.php
+++ config/google.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 return [
     'enabled' => env('GOOGLE_ENABLED', false),
-    'credentials' => json_decode(env('GOOGLE_CREDENTIALS'), true),
+    'credentials' => json_decode(env('GOOGLE_CREDENTIALS', '{}'), true),
     'admin' => env('GOOGLE_ADMIN'),
     'manual_groups' => explode(',', env('GOOGLE_MANUAL_GROUPS', '')),
 ];
