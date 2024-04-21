d223427b76aa333fbf824532a9e7035230fb78c7
Kristaps Berzinch <kristapsberzinch@gmail.com>
2024-04-21 16:23:06 -0400

    Suppress login throttling analyzer, seems to be false positive

--- config/enlightn.php
+++ config/enlightn.php
@@ -123,6 +123,7 @@ return [
         ] : []),
         \Enlightn\Enlightn\Analyzers\Security\StableDependencyAnalyzer::class,
         \Enlightn\Enlightn\Analyzers\Security\VulnerableDependencyAnalyzer::class,
+        \Enlightn\Enlightn\Analyzers\Security\LoginThrottlingAnalyzer::class,
     ],
 
     /*
