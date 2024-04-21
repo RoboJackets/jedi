3428a246d2d70687e9bf41c61bb75297b4cce498
Shift <shift@laravelshift.com>
2024-04-21 19:28:09 +0000

    Re-register exception handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use App\Providers\AppServiceProvider;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withProviders([
@@ -38,5 +39,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->reportable(static function (Throwable $e): void {
+            Integration::captureUnhandledException($e);
+        });
     })->create();
