c947d71875c7bfdf15b9029929b2926b307e7c3a
Kristaps Berzinch <kristapsberzinch@gmail.com>
2024-04-21 15:51:33 -0400

    Clean up AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
@@ -7,20 +9,11 @@ use Illuminate\Support\ServiceProvider;
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * The path to your application's "home" route.
-     *
-     * Typically, users are redirected here after authentication.
-     *
-     * @var string
-     */
-    public const HOME = '/home';
-
-    /**
      * Register any application services.
      */
     public function register(): void
     {
-        //
+        // nothing to do here
     }
 
     /**
@@ -28,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // nothing to do here
     }
 }
