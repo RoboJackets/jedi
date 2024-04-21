ce08fa9fad9a3552e001153281dfef1a47bfe549
Shift <shift@laravelshift.com>
2024-04-21 19:28:09 +0000

    Re-register service providers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,7 +6,11 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withProviders()
+    ->withProviders([
+        \Subfission\Cas\CasServiceProvider::class,
+        \Illuminate\Foundation\Support\Providers\AuthServiceProvider::class,
+        \Illuminate\Foundation\Support\Providers\EventServiceProvider::class,
+    ])
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         // api: __DIR__.'/../routes/api.php',
--- config/app.php
+++ config/app.php
@@ -7,24 +7,6 @@ use Illuminate\Support\Facades\Facade;
 
 return [
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Laravel Framework Service Providers...
-         */
-
-        /*
-         * Package Service Providers...
-         */
-        Subfission\Cas\CasServiceProvider::class,
-
-        /*
-         * Application Service Providers...
-         */
-        Illuminate\Foundation\Support\Providers\AuthServiceProvider::class,
-        Illuminate\Foundation\Support\Providers\EventServiceProvider::class,
-        App\Providers\HorizonServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-    ])->toArray(),
 
     'aliases' => Facade::defaultAliases()->merge([
         'Cas' => Subfission\Cas\Facades\Cas::class,
